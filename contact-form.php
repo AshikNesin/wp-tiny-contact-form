91c8f5f2cd0947da1badc968734db07ca040b804
Ashik Nesin <ashik@chargebee.com>
2022-07-02 13:10:04 +0530

    rename shortcode and plugin's meta

--- contact-form.php
+++ contact-form.php
@@ -1,32 +1,32 @@
 <?php
 /*
-Plugin Name: HugeThoughts's Contact Form
-Plugin URI: http://www.hugethoughts.com
-Description: Simple WordPress Contact Form which uses less resources when compared with others.
-Version: 1.0
+Plugin Name: Tiny Contact Form
+Plugin URI: https://github.com/AshikNesin/wp-tiny-contact-form
+Description: Simple no-configuration contact form for your WordPress
+Version: 0.1
 Author: Ashik Nesin
-Author URI: http://www.AshikNesin.com
+Author URI: https://nesin.io
 */
  
 function html_form_code() {
     echo '<form action="' . esc_url( $_SERVER['REQUEST_URI'] ) . '" method="post">';
     echo '<p>';
     echo 'Your Name (required) <br/>';
-    echo '<input type="text" name="hts-name" pattern="[a-zA-Z0-9 ]+" value="' . ( isset( $_POST["hts-name"] ) ? esc_attr( $_POST["hts-name"] ) : '' ) . '" size="40" />';
+    echo '<input type="text" name="tiny-name" pattern="[a-zA-Z0-9 ]+" value="' . ( isset( $_POST["tiny-name"] ) ? esc_attr( $_POST["tiny-name"] ) : '' ) . '" size="40" />';
     echo '</p>';
     echo '<p>';
     echo 'Your Email (required) <br/>';
-    echo '<input type="email" name="hts-email" value="' . ( isset( $_POST["hts-email"] ) ? esc_attr( $_POST["hts-email"] ) : '' ) . '" size="40" />';
+    echo '<input type="email" name="tiny-email" value="' . ( isset( $_POST["tiny-email"] ) ? esc_attr( $_POST["tiny-email"] ) : '' ) . '" size="40" />';
     echo '</p>';
     echo '<p>';
     echo 'Subject (required) <br/>';
-    echo '<input type="text" name="hts-subject" pattern="[a-zA-Z ]+" value="' . ( isset( $_POST["hts-subject"] ) ? esc_attr( $_POST["hts-subject"] ) : '' ) . '" size="40" />';
+    echo '<input type="text" name="tiny-subject" pattern="[a-zA-Z ]+" value="' . ( isset( $_POST["tiny-subject"] ) ? esc_attr( $_POST["tiny-subject"] ) : '' ) . '" size="40" />';
     echo '</p>';
     echo '<p>';
     echo 'Your Message (required) <br/>';
-    echo '<textarea rows="10" cols="35" name="hts-message">' . ( isset( $_POST["hts-message"] ) ? esc_attr( $_POST["hts-message"] ) : '' ) . '</textarea>';
+    echo '<textarea rows="10" cols="35" name="tiny-message">' . ( isset( $_POST["tiny-message"] ) ? esc_attr( $_POST["tiny-message"] ) : '' ) . '</textarea>';
     echo '</p>';
-    echo '<p><input type="submit" name="hts-submitted" value="Send"></p>';
+    echo '<p><input type="submit" name="tiny-submitted" value="Send"></p>';
     echo '</form>';
 //Clear the TextBox in Contact Form --- Have to implement it..
 	  
@@ -35,13 +35,13 @@ function html_form_code() {
 function deliver_mail() {
  
     // if the submit button is clicked, send the email
-    if ( isset( $_POST['hts-submitted'] ) ) {
+    if ( isset( $_POST['tiny-submitted'] ) ) {
  
         // sanitize form values
-        $name    = sanitize_text_field( $_POST["hts-name"] );
-        $email   = sanitize_email( $_POST["hts-email"] );
-        $subject = sanitize_text_field( $_POST["hts-subject"] );
-        $message = esc_textarea( $_POST["hts-message"] );
+        $name    = sanitize_text_field( $_POST["tiny-name"] );
+        $email   = sanitize_email( $_POST["tiny-email"] );
+        $subject = sanitize_text_field( $_POST["tiny-subject"] );
+        $message = esc_textarea( $_POST["tiny-message"] );
  
         // get the blog administrator's email address
         $to = get_option( 'admin_email' );
@@ -61,7 +61,7 @@ function deliver_mail() {
     }
 }
  
-function hts_shortcode() {
+function tiny_shortcode() {
     ob_start();
     deliver_mail();
     html_form_code();
@@ -69,6 +69,6 @@ function hts_shortcode() {
     return ob_get_clean();
 }
  
-add_shortcode( 'hugethoughts_contact_form', 'hts_shortcode' );
+add_shortcode( 'tiny_contact_form', 'tiny_shortcode' );
  
 ?>
